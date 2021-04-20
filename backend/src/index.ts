d40077aad1754e0fcac069a29e3d1d32a635c458
Alex Speranza <alex.speranza@studio.unibo.it>
2021-04-20 20:30:45 +0200

    Changes message to JSON

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, world!");
+    res.status(200).json({ message: "Hello, world!"});
 });
 
 app.listen(PORT, () => {
