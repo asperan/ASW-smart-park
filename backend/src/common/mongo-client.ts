94ae4805b9b64faa46ca6658eb51e1483b7bb735
Kyrillos Ntronov <ntronov.kyrillos@gmail.com>
2021-05-03 23:35:58 +0200

    add mongo wrapper

---
+++ backend/src/common/mongo-client.ts
@@ -0,0 +1,31 @@
+const MongoClient = require('mongodb').MongoClient
+
+class MongoWrapper {
+
+    db: any;
+
+    constructor() {
+
+    }
+
+    async connect(uri: string, dbName: string) {
+        await new Promise((resolve, reject) => {
+            MongoClient.connect(
+                uri,
+                { useNewUrlParser: true, useUnifiedTopology: true },
+                (error: any, client: any) => {
+                    if (error) {
+                        reject(error)
+                    }
+
+                    console.log("Connected successfully to Mongodb");
+
+                    this.db = client.db(dbName);
+                    resolve(this.db);
+                }
+            );
+        }).catch((error) => { throw error});
+    }
+}
+
+export let MongoConnection = new MongoWrapper();
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,41 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoConnection } from './common/mongo-client';
+import { mainModule } from 'node:process';
 
 const app = express();
 const cityRoutes = require('./routes/city-routes');
 const parkingRoutes = require('./routes/parking-routes');
 
-app.use(express.json());
-app.use(cors());
-
 const PORT = 3000;
+const DB_URI = "";
+const DB_NAME = "";
 
-app.use('/api/city', cityRoutes);
-app.use('/api/parking', parkingRoutes);
+main();
 
-app.listen(PORT, () => {
-    console.log("Listening on port " + PORT);
-});
+function main() {
+    connectToMongoDB();
+    configureMiddleware();
+    configureRoutes();
+    startServer();
+}
+
+function connectToMongoDB() {
+    MongoConnection.connect(DB_URI, DB_NAME);
+}
+
+function configureMiddleware() {
+    app.use(express.json());
+    app.use(cors());
+}
+
+function configureRoutes() {
+    app.use('/api/city', cityRoutes);
+    app.use('/api/parking', parkingRoutes);
+}
+
+function startServer() {
+    app.listen(PORT, () => {
+        console.log("Listening on port " + PORT);
+    });
+}
