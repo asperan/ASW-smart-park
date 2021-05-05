37a7b02e0bf1c64f48f00cc15c50bd1233ca7832
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-05 13:07:33 +0200

    Improves import and export statements

--- backend/src/controllers/parking-controller.ts
+++ backend/src/controllers/parking-controller.ts
@@ -1,23 +1,16 @@
 import { Coordinates } from "../data/Coordinates";
 import { ParkingDto } from "../dto/parking-dto";
 import { Parking } from "../models/parking-model";
+import * as parkingService from "../services/parking-service";
 
-const parkingService = require("../services/parking-service");
-
-function getParkingInCity(cityId: number): Array<ParkingDto> {
+export function getParkingInCity(cityId: number): Array<ParkingDto> {
     return parkingService.findAvailableParkingByCityId(cityId).map((p: Parking) => ParkingDto.fromModel(p));
 }
 
-function getParkingInCityIdWithinRadiusFromPoint(cityId: number, center: Coordinates, radiusKm: number): Array<ParkingDto> {
+export function getParkingInCityIdWithinRadiusFromPoint(cityId: number, center: Coordinates, radiusKm: number): Array<ParkingDto> {
     return parkingService.findAvailableParkingByCityIdWithinRadiusFromPoint(cityId, center, radiusKm).map((p: Parking) => ParkingDto.fromModel(p));
 }
 
-function getAvailableParkingByCityIdWithinRadiusFromCityCenter(cityId: number, radiusKm: number): Array<ParkingDto> {
+export function getAvailableParkingByCityIdWithinRadiusFromCityCenter(cityId: number, radiusKm: number): Array<ParkingDto> {
     return parkingService.findAvailableParkingByCityIdWithinRadiusFromCityCenter(cityId, radiusKm).map((p: Parking) => ParkingDto.fromModel(p));
-}
-
-module.exports = {
-    getParkingInCity,
-    getParkingInCityIdWithinRadiusFromPoint,
-    getAvailableParkingByCityIdWithinRadiusFromCityCenter
 }
