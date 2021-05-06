19557cadbb664ac1975a64f290e5b2245c3a0a75
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-06 22:45:57 +0200

    Changes class into type declaration

--- backend/src/models/city-model.ts
+++ backend/src/models/city-model.ts
@@ -1,58 +1,45 @@
 import { Coordinates } from "../data/Coordinates";
 
-export class City {
-
-    id: number;
-    name: string;
-    coordinates: Coordinates;
-
-    constructor(id: number, name: string, coordinates: Coordinates) {
-        this.id = id;
-        this.name = name;
-        this.coordinates = coordinates;
-    }
+export type CityModel = {
+    id: number,
+    name: string,
+    coordinates: Coordinates,
 }
 
-
-
-
-// Mocking DB interactions with static objects 
-// only temporary until mongo is integrated :)
-
-const CITIES = {
-    "Cesena": new City(
-        1,
-        "Cesena",
-        new Coordinates(
-            44.1390945,
-            12.2429281
-        )
-    ),
-    "Rimini": new City(
-        2,
-        "Rimini",
-        new Coordinates(
-            44.059444,
-            12.568333
-        )
-    )
-}
-
-export function findAllCities(): Array<City> {
+export function findAllCities(): Array<CityModel> {
     return [
         CITIES.Cesena,
         CITIES.Rimini
     ];
 }
 
-export function findCityById(id: number): City {
+export function findCityById(id: number): CityModel {
     return CITIES.Cesena;
 }
 
-export function findCitiesByPartialName(name: String): Array<City> {
+export function findCitiesByPartialName(name: String): Array<CityModel> {
     if (name.startsWith("C")) {
         return [CITIES.Cesena];
     } else {
         return [CITIES.Rimini];
+    }
+}
+
+const CITIES = {
+    "Cesena": {
+        id: 1,
+        name: "Cesena",
+        coordinates: {
+            longitude: 44.1390945,
+            latitude: 12.2429281
+        }
+    },
+    "Rimini": {
+        id: 2,
+        name: "Rimini",
+        coordinates: {
+            longitude: 44.059444,
+            latitude: 12.568333
+        }
     }
 }
