ef5baee66cfe6ebe9958e7682715fe53176ba49b
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-06 22:46:28 +0200

    Changes used types

--- backend/src/services/city-service.ts
+++ backend/src/services/city-service.ts
@@ -1,13 +1,13 @@
 import * as cityModel from "../models/city-model";
 
-export function suggestCityByPartialName(name: String): Array<cityModel.City> {
+export function suggestCityByPartialName(name: String): Array<cityModel.CityModel> {
     return cityModel.findCitiesByPartialName(name);
 }
 
-export function getCityById(cityId: number): cityModel.City {
+export function getCityById(cityId: number): cityModel.CityModel {
     return cityModel.findCityById(cityId);
 }
 
-export function getAllCities(): Array<cityModel.City> {
+export function getAllCities(): Array<cityModel.CityModel> {
     return cityModel.findAllCities();
 }
