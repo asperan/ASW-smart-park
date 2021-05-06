3298603718ac2f4f51b97b41b027ec8eeffb2834
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-06 22:46:38 +0200

    Changes used types

--- backend/src/controllers/parking-controller.ts
+++ backend/src/controllers/parking-controller.ts
@@ -1,16 +1,16 @@
 import { Coordinates } from "../data/Coordinates";
-import { ParkingDto } from "../dto/parking-dto";
+import { ParkingDto, DtoFromModel } from "../dto/parking-dto";
 import { Parking } from "../models/parking-model";
 import * as parkingService from "../services/parking-service";
 
 export function getParkingInCity(cityId: number): Array<ParkingDto> {
-    return parkingService.findAvailableParkingByCityId(cityId).map((p: Parking) => ParkingDto.fromModel(p));
+    return parkingService.findAvailableParkingByCityId(cityId).map((p: Parking) => DtoFromModel(p));
 }
 
 export function getParkingInCityIdWithinRadiusFromPoint(cityId: number, center: Coordinates, radiusKm: number): Array<ParkingDto> {
-    return parkingService.findAvailableParkingByCityIdWithinRadiusFromPoint(cityId, center, radiusKm).map((p: Parking) => ParkingDto.fromModel(p));
+    return parkingService.findAvailableParkingByCityIdWithinRadiusFromPoint(cityId, center, radiusKm).map((p: Parking) => DtoFromModel(p));
 }
 
 export function getAvailableParkingByCityIdWithinRadiusFromCityCenter(cityId: number, radiusKm: number): Array<ParkingDto> {
-    return parkingService.findAvailableParkingByCityIdWithinRadiusFromCityCenter(cityId, radiusKm).map((p: Parking) => ParkingDto.fromModel(p));
+    return parkingService.findAvailableParkingByCityIdWithinRadiusFromCityCenter(cityId, radiusKm).map((p: Parking) => DtoFromModel(p));
 }
