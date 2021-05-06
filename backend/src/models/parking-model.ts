10afc5cfdcb71f0db92b336315e566a777ca0265
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-06 22:46:06 +0200

    Changes class into type declaration

--- backend/src/models/parking-model.ts
+++ backend/src/models/parking-model.ts
@@ -1,33 +1,57 @@
-import { timeStamp } from "node:console";
 import { Coordinates } from "../data/Coordinates";
 
-export class Parking {
+export type Parking = {
 
     idParking: number;
     parentCityId: number;
     capacity: number;
     currentOccupancy: number;
     coordinates: Coordinates;
-
-    constructor(idParking: number, parentCityId: number, capacity: number, currentOccupancy: number, coordinates: Coordinates) {
-        this.idParking = idParking;
-        this.parentCityId = parentCityId;
-        this.capacity = capacity;
-        this.currentOccupancy = currentOccupancy;
-        this.coordinates = coordinates;
-    }
 }
-
-// Mocking DB interactions with static objects 
-// only temporary until mongo is integrated :)
-
-const parkings = [
-    new Parking(1, 1, 20, 0, new Coordinates(44.147660, 12.236731)),
-    new Parking(2, 1, 50, 5, new Coordinates(44.144281, 12.238529)),
-    new Parking(3, 1, 50, 5, new Coordinates(44.144281, 12.238529)),
-    new Parking(4, 1, 100, 100, new Coordinates(44.143680, 12.247764))
-]
 
 export function findParkingsByCityId(cityId: number): Array<Parking> {
     return parkings;
 }
+
+const parkings = [
+    {
+        idParking: 1,
+        parentCityId: 1,
+        capacity: 20,
+        currentOccupancy: 0,
+        coordinates: {
+            longitude: 44.147660,
+            latitude: 12.236731
+        }
+    },
+    {
+        idParking: 2,
+        parentCityId: 1,
+        capacity: 50,
+        currentOccupancy: 5,
+        coordinates: {
+            longitude: 44.144281,
+            latitude: 12.238529
+        }
+    },
+    {
+        idParking: 3,
+        parentCityId: 1,
+        capacity: 50,
+        currentOccupancy: 5,
+        coordinates: {
+            longitude: 44.144281,
+            latitude: 12.238529
+        }
+    },
+    {
+        idParking: 4,
+        parentCityId: 1,
+        capacity: 100,
+        currentOccupancy: 100,
+        coordinates: {
+            longitude: 44.143680,
+            latitude: 12.247764
+        }
+    }
+]
