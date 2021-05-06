09e1289cc8a0d66e995111e3a572d2d6211b75e3
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-06 22:46:45 +0200

    Changes used types

--- backend/src/controllers/city-controller.ts
+++ backend/src/controllers/city-controller.ts
@@ -1,7 +1,7 @@
-import { CityDto } from "../dto/city-dto";
-import { City } from "../models/city-model";
+import { CityDto, DtoFromModel } from "../dto/city-dto";
+import { CityModel } from "../models/city-model";
 import * as cityService from "../services/city-service";
 
 export function suggestCity(partialName: String): Array<CityDto> {
-    return cityService.suggestCityByPartialName(partialName).map((c: City) => CityDto.fromModel(c));
+    return cityService.suggestCityByPartialName(partialName).map((c: CityModel) => DtoFromModel(c));
 }
