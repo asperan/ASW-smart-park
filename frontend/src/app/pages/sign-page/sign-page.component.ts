6f746593e0cf1d358597ef0bee46184a77c05a85
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-10 22:10:21 +0200

    Implements most of sign in/up process

--- frontend/src/app/pages/sign-page/sign-page.component.ts
+++ frontend/src/app/pages/sign-page/sign-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap} from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
+import { SignupService } from './sign-services/signup.service';
+import { SigninService } from "./sign-services/signin.service";
+import UserCredentials from './sign-services/user-credentials';
+import * as SignErrors from "./sign-errors";
 
 @Component({
   selector: 'app-sign-page',
@@ -9,17 +13,57 @@ import { Router, ActivatedRoute, ParamMap} from "@angular/router";
 export class SignPageComponent implements OnInit {
 
   isSignup!: boolean;
+  signError: SignErrors.SignError;
+  errorMessage: string;
 
-  constructor(private route: ActivatedRoute, ) { }
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router, 
+    private signupService: SignupService,
+    private signinService: SigninService) { 
+      this.signError = SignErrors.Common.ServerError;
+      this.errorMessage = SignErrors.errorMessages[this.signError];
+    }
 
-  onSubmit(data: Object) {
-    console.log(data);
-    // TODO: 
-    // Use service to login
+  onSubmit(data: any) {
+    const userCredentials: UserCredentials = { email: data.email, password: data.password };
+    if(this.isSignup) {
+      this.onSubmitSignup(userCredentials);
+    } else {
+      this.onSubmitSignin(userCredentials);
+    }
   }
 
   ngOnInit(): void {
     this.route.data.subscribe(data => this.isSignup = data.isSignup);
   }
 
+  private onSubmitSignup(userCredentials: UserCredentials): void {
+    const router = this.router;
+    this.signupService.requestSignup(userCredentials).then(data => {
+      if (data.code > 0) {
+        this.onRequestError(data);
+      } else {
+        alert("You are subscribed! You will be redirected to sing in page where you can log in.");
+        router.navigate(['signin']);
+      }
+    });
+  }
+
+  private onSubmitSignin(userCredentials: UserCredentials): void {
+    const router = this.router;
+    this.signinService.requestSignin(userCredentials).then(data => {
+      if (data.code > 0) {
+        this.onRequestError(data);
+      } else {
+        // TODO:
+        // Redirect to user page
+      }
+    });
+  }
+
+  private onRequestError(error: any) {
+    this.signError = error.code;
+    this.errorMessage = SignErrors.errorMessages[this.signError];
+  }
 }
