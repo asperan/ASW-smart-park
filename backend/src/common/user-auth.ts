fee6649fef155821264e209a7759341b6193870d
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-11 12:58:39 +0200

    Removes unused function

--- backend/src/common/user-auth.ts
+++ backend/src/common/user-auth.ts
@@ -1,17 +1,10 @@
 import CryptoJs from "crypto-js";
 import cryptoRandomString from "crypto-random-string";
-import { MongoConnection } from "./mongo-client";
 
 const hashSecretKey = process.env.HASHING_KEY || "";
 
 export function hashPassword(password: string, salt: string): string {
   return CryptoJs.HmacSHA256(salt + password + salt, hashSecretKey).toString();
-}
-
-// Checks in the db if the email is present
-export async function isUserPresent(email: string): Promise<boolean> {
-  const userCollection = MongoConnection.db.collection("users");
-  return userCollection.countDocuments({email: email}) > 0;
 }
 
 export function generateNewSalt(): string {
