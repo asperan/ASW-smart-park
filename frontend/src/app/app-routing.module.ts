1c4a1cb857c3faa219fc712ea06e87abd91d3546
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-13 16:25:43 +0200

    Adds route for the user page component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SignPageComponent } from './pages/sign-page/sign-page.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
 
 const routes: Routes = [
   {path: "", component: AppComponent}, 
   {path: "signup", component: SignPageComponent, data: {isSignup: true}},
   {path: "signin", component: SignPageComponent, data: {isSignup: false}},
+  {path: "userpage", component: UserPageComponent},
 ];
 
 @NgModule({
