05c996ba0267b08ef4874f126817d465c792d3b0
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-14 15:52:39 +0200

    Adds new components to app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,13 +7,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignPageComponent } from './pages/sign-page/sign-page.component';
 import { UserPageComponent } from './pages/user-page/user-page.component';
+import { VehicleTabComponent } from './pages/user-page/vehicle-tab/vehicle-tab.component';
+import { PaymentTabComponent } from './pages/user-page/payment-tab/payment-tab.component';
+import { StatisticsTabComponent } from './pages/user-page/statistics-tab/statistics-tab.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SignPageComponent,
-    UserPageComponent
+    UserPageComponent,
+    VehicleTabComponent,
+    PaymentTabComponent,
+    StatisticsTabComponent
   ],
   imports: [
     BrowserModule,
