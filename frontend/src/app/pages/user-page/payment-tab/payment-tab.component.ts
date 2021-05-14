62fda2cf78b3a682d5c334292cf5ad6df97dbf6f
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-14 15:52:08 +0200

    Adds user payments page

---
+++ frontend/src/app/pages/user-page/payment-tab/payment-tab.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-tab',
+  templateUrl: './payment-tab.component.html',
+  styleUrls: ['./payment-tab.component.css']
+})
+export class PaymentTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("Payment tab init");
+  }
+
+}
