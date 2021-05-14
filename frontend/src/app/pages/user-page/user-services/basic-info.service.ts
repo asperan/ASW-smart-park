b5de621a4e77c3f5f555e1dbd08e783b9c01766e
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-14 17:27:38 +0200

    Adds injection of TokenManagerService

--- frontend/src/app/pages/user-page/user-services/basic-info.service.ts
+++ frontend/src/app/pages/user-page/user-services/basic-info.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { getToken } from "src/app/access-token/token-manager";
+import { TokenManagerService } from "src/app/access-token/token-manager";
 
 @Injectable({
   providedIn: "root",
 })
 export class BasicInfoService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private tokenManagerService: TokenManagerService) {}
 
   requestBasicInfos(): Promise<any> {
-    return this.http.post("http://localhost:3000/api/user-info/basic", {}, {headers: {"x-access-token": getToken()}}).toPromise();
+    return this.http.post("http://localhost:3000/api/user-info/basic", {}, {headers: {"x-access-token": this.tokenManagerService.getToken()}}).toPromise();
   }
 }
