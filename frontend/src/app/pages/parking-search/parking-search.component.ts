2024ce6410c4850bbf859b2c4726471b8f7158d8
Kyrillos Ntronov <ntronov.kyrillos@gmail.com>
2021-05-17 22:18:57 +0200

    add basic leaflet map frame

--- frontend/src/app/pages/parking-search/parking-search.component.ts
+++ frontend/src/app/pages/parking-search/parking-search.component.ts
@@ -1,18 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-parking-search',
   templateUrl: './parking-search.component.html',
   styleUrls: ['./parking-search.component.css']
 })
-export class ParkingSearchComponent implements OnInit {
+export class ParkingSearchComponent implements AfterViewInit {
 
   faUser = faUser;
 
-  constructor() { }
+  private map: any;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.initMap();
   }
 
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  
 }
