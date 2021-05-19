569f8fe418c123b8d7c48f66e45a4bbe9958d26b
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-19 15:56:23 +0200

    Adds function to send a json with the list of linked vehicles

--- backend/src/controllers/user-info-controller.ts
+++ backend/src/controllers/user-info-controller.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { isJwtCorrect } from "../services/user-auth";
+import * as userService from "../services/user-service";
 
 export function getBasicUserInfo(request: express.Request, response: express.Response) {
   const accessToken = request.header("x-access-token");
@@ -7,6 +8,20 @@ export function getBasicUserInfo(request: express.Request, response: express.Res
     const result = isJwtCorrect(accessToken);
     if (result.ok) {
       response.status(200).json({ code: 0, email: result.email });
+    } else {
+      response.status(400).json({ code: 1, message: "Bad JWT." });
+    }
+  } else {
+    response.status(400).json({ code: 2, message: "JWT not sent." });
+  }
+}
+
+export function getVehicleUserInfo(request: express.Request, response: express.Response) {
+  const accessToken = request.header("x-access-token");
+  if (accessToken) {
+    const result = isJwtCorrect(accessToken);
+    if (result.ok && result.email) {
+      userService.getVehicleUserInfo(result.email).then(data => response.status(200).json(data));
     } else {
       response.status(400).json({ code: 1, message: "Bad JWT." });
     }
