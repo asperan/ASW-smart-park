9dce8df4108184c427e16052844f6767438dccf7
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-19 15:56:45 +0200

    Adds route to get the user-specific vehicle infos

--- backend/src/routes/user-info-routes.ts
+++ backend/src/routes/user-info-routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import { getBasicUserInfo } from "../controllers/user-info-controller";
+import { getBasicUserInfo, getVehicleUserInfo } from "../controllers/user-info-controller";
 
 export function setUserInfoRoutes(app: express.Application): void {
   app.post("/api/user-info/basic", getBasicUserInfo);
+  app.post("/api/user-info/vehicles", getVehicleUserInfo);
 }
