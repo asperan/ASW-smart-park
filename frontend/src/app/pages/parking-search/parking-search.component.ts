6af565dcfab48dd22b3a5be14c26ff3ce0db1291
Kyrillos Ntronov <ntronov.kyrillos@gmail.com>
2021-05-20 19:19:40 +0200

    add map markers support and geolocation with radius

--- frontend/src/app/pages/parking-search/parking-search.component.ts
+++ frontend/src/app/pages/parking-search/parking-search.component.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCircle, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
 import * as L from 'leaflet';
 
 @Component({
@@ -9,20 +9,51 @@ import * as L from 'leaflet';
 })
 export class ParkingSearchComponent implements AfterViewInit {
 
+  // TODO add button to geolocate
+  // TODO fix marker not showing
+  // TODO add range slider
+  // TODO bind backend
+
   faUser = faUser;
   faSearch = faSearch;
 
+  private currentLocationMarker: L.Marker<any> | undefined;
+  private searchRadiusMarker: L.Circle<any> | undefined;
+
+  private searchLocation: L.LatLngExpression | undefined;
+  private searchRange = 1000;
+
+  private navIcon = L.icon({
+    iconUrl: '/assets/images/nav-icon.png',
+    iconSize: [35, 20]
+  });
+
   private map: any;
 
-  constructor() {}
+  constructor() {
+  }
 
   ngAfterViewInit(): void {
     this.initMap();
   }
 
+  locatePosition() {
+    this.map.locate({ setView: true, maxZoom: 16 });
+  }
+
+  onRangeChange(value: string) {
+    switch(value) {
+      case "1": this.searchRange = 250; break;
+      case "2": this.searchRange = 500; break;
+      case "3": this.searchRange = 1000; break;
+      case "4": this.searchRange = 5000; break;
+      case "5": this.searchRange = 10000; break;
+    }
+    this.updateMarkers();
+  }
+
   private initMap(): void {
     this.map = L.map('map', {
-      center: [ 41.890572, 12.494314],
       zoom: 13
     });
 
@@ -33,7 +64,34 @@ export class ParkingSearchComponent implements AfterViewInit {
     });
 
     tiles.addTo(this.map);
+
+    this.map.on('locationfound', (e: { accuracy: any; latlng: L.LatLngExpression; }) => {
+      this.searchLocation = e.latlng;
+      this.updateMarkers();
+    });
+
+    this.map.on('locationerror', (e: { message: any; }) => {
+      alert("Ooops, couldn't get your current position! Please re-try or select a city ;)");
+    });
+
+    this.locatePosition();
   }
 
-  
+  private updateMarkers() {
+    if(this.searchLocation) {
+      this.clearMarkers();
+      this.currentLocationMarker = L.marker(this.searchLocation, { icon: this.navIcon }).addTo(this.map);
+      this.searchRadiusMarker = L.circle(this.searchLocation, this.searchRange, {color: "blue", opacity:.5, fill: false}).addTo(this.map);
+    }
+  }
+
+  private clearMarkers() {
+    if(this.currentLocationMarker) {
+      this.map.removeLayer(this.currentLocationMarker);
+    }
+    if(this.searchRadiusMarker) {
+      this.map.removeLayer(this.searchRadiusMarker);
+    }
+  }
+
 }
