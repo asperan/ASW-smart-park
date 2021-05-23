23322d7b04aedf7a47e780f5f995b9115427db2c
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-23 17:01:48 +0200

    Adds route for user payment infos

--- backend/src/routes/user-info-routes.ts
+++ backend/src/routes/user-info-routes.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { getBasicUserInfo, getVehicleUserInfo } from "../controllers/user-info-controller";
+import { getBasicUserInfo, getUserPaymentsInfo, getVehicleUserInfo } from "../controllers/user-info-controller";
 
 export function setUserInfoRoutes(app: express.Application): void {
   app.post("/api/user-info/basic", getBasicUserInfo);
   app.post("/api/user-info/vehicles", getVehicleUserInfo);
+  app.post("/api/user-info/payments", getUserPaymentsInfo);
 }
