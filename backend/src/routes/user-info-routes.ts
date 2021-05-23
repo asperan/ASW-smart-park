f8132c6c208b7bab9ab8b27affc1de163ddab2d3
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-23 18:56:39 +0200

    Adds route for user statistics

--- backend/src/routes/user-info-routes.ts
+++ backend/src/routes/user-info-routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { getBasicUserInfo, getUserPaymentsInfo, getVehicleUserInfo } from "../controllers/user-info-controller";
+import { getBasicUserInfo, getUserPaymentsInfo, getUserStatistics, getVehicleUserInfo } from "../controllers/user-info-controller";
 
 export function setUserInfoRoutes(app: express.Application): void {
   app.post("/api/user-info/basic", getBasicUserInfo);
   app.post("/api/user-info/vehicles", getVehicleUserInfo);
   app.post("/api/user-info/payments", getUserPaymentsInfo);
+  app.post("/api/user-info/stats", getUserStatistics);
 }
