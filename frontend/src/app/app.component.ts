2b520e7134963bd23a3926c1bfbefdea5c6159ac
Francesco Agnoletti <francesco.agnoletti3@studio.unibo.it>
2021-05-24 11:57:14 +0100

    Separated header and footer components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,8 +3,18 @@ import { SimpleService } from './simple-service.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ // templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  template: `
+    <!-- Header -->
+    <app-header></app-header>
+
+    <!-- routes will be rendered here -->
+    <router-outlet></router-outlet>
+
+    <!-- Footer -->
+    <app-footer></app-footer>
+  `,
 })
 export class AppComponent {
   title = 'smart-parking';
