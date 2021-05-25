670895b0485cc5ff161241975a6dee5501f7ac83
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-25 10:23:40 +0200

    Adds TODOs

--- backend/src/routes/user-info-routes.ts
+++ backend/src/routes/user-info-routes.ts
@@ -3,7 +3,9 @@ import { getBasicUserInfo, getUserPaymentsInfo, getUserStatistics, getVehicleUse
 
 export function setUserInfoRoutes(app: express.Application): void {
   app.get("/api/user-info/basic", getBasicUserInfo);
+  // TODO: add route to post a new vehicle
   app.get("/api/user-info/vehicles", getVehicleUserInfo);
+  // TODO: add route to post a new payment
   app.get("/api/user-info/payments", getUserPaymentsInfo);
   app.get("/api/user-info/stats", getUserStatistics);
 }
