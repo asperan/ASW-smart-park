eb07b396c5221ef2068bb43914b21061b98a6e1d
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-25 19:04:11 +0200

    Adds routes for managing payments

--- backend/src/routes/user-info-routes.ts
+++ backend/src/routes/user-info-routes.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { getBasicUserInfo, getUserPaymentsInfo, getUserStatistics, getVehicleUserInfo, postUserVehicle } from "../controllers/user-info-controller";
+import { addUserPayment, getBasicUserInfo, getUserPaymentsInfo, getUserStatistics, getVehicleUserInfo, postUserVehicle, resolvePendingPayment } from "../controllers/user-info-controller";
 
 export function setUserInfoRoutes(app: express.Application): void {
   app.get("/api/user-info/basic", getBasicUserInfo);
   app.get("/api/user-info/vehicles", getVehicleUserInfo)
      .post("/api/user-info/vehicles", postUserVehicle);
-  // TODO: add route to post a new payment
-  app.get("/api/user-info/payments", getUserPaymentsInfo);
+  app.get("/api/user-info/payments", getUserPaymentsInfo)
+  .post("/api/user-info/payments", addUserPayment)
+  .put("/api/user-info/payments", resolvePendingPayment);
   app.get("/api/user-info/stats", getUserStatistics);
 }
