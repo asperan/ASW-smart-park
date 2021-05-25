b373447583b68b031861367852a47ad1d00361bb
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-25 12:22:16 +0200

    Adds function to add a vehicle to a user

--- backend/src/services/user-service.ts
+++ backend/src/services/user-service.ts
@@ -1,5 +1,6 @@
 import * as userRepository from "../repositories/users-repository";
 import { InsertOneWriteOpResult } from "mongodb";
+import * as vehicleService from "./vehicle-service";
 
 export async function isUserAlreadyPresent(email: string): Promise<boolean> {
   return userRepository.isUserAlreadyPresent(email);
@@ -15,6 +16,22 @@ export async function checkUserPassword(email: string, password: string): Promis
 
 export async function getVehicleUserInfo(email:string) {
   return userRepository.getUserLinkedVehicles(email);
+}
+
+export async function addUserVehicle(email: string, vehicleId: string, vehicleName: string): Promise<number> {
+  if (!(await vehicleService.isVehiclePresent(vehicleId))) {
+    const vehicleInsertionResult = await vehicleService.insertVehicle(vehicleId);
+    if (!vehicleInsertionResult) { return 0; }
+  }
+  if (!(await isVehicleLinked(email, vehicleId))) {
+    const linkVehicleResult = await userRepository.linkVehicle(email, vehicleId, vehicleName);
+    if (!linkVehicleResult) { return 0; }
+  } else { return 2; } // Vehicle already linked
+  return 1; // Everything OK
+}
+
+export async function isVehicleLinked(email: string, vehicleId: string) {
+  return userRepository.isVehicleLinked(email, vehicleId);
 }
 
 export async function getUserPaymentsInfo(email: string) {
