4fcddf4fc6f12c59f21b1d61db28933fe475bb0f
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-25 12:23:22 +0200

    Adds route to add a vehicle to a user

--- backend/src/routes/user-info-routes.ts
+++ backend/src/routes/user-info-routes.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { getBasicUserInfo, getUserPaymentsInfo, getUserStatistics, getVehicleUserInfo } from "../controllers/user-info-controller";
+import { getBasicUserInfo, getUserPaymentsInfo, getUserStatistics, getVehicleUserInfo, postUserVehicle } from "../controllers/user-info-controller";
 
 export function setUserInfoRoutes(app: express.Application): void {
   app.get("/api/user-info/basic", getBasicUserInfo);
-  // TODO: add route to post a new vehicle
-  app.get("/api/user-info/vehicles", getVehicleUserInfo);
+  app.get("/api/user-info/vehicles", getVehicleUserInfo)
+     .post("/api/user-info/vehicles", postUserVehicle);
   // TODO: add route to post a new payment
   app.get("/api/user-info/payments", getUserPaymentsInfo);
   app.get("/api/user-info/stats", getUserStatistics);
