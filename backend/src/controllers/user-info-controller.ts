86ce8498a3b5712323512fd440f40a2e3cf2a74b
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-25 11:27:48 +0200

    Adds utility function to check access token and retrieve email

--- backend/src/controllers/user-info-controller.ts
+++ backend/src/controllers/user-info-controller.ts
@@ -3,53 +3,31 @@ import { isJwtCorrect } from "../services/user-auth";
 import * as userService from "../services/user-service";
 
 export function getBasicUserInfo(request: express.Request, response: express.Response) {
-  const accessToken = request.header("x-access-token");
-  if (accessToken) {
-    const result = isJwtCorrect(accessToken);
-    if (result.ok && result.email) {
-      response.status(200).json({ code: 0, email: result.email });
-    } else {
-      response.status(400).json({ code: 1, message: "Bad JWT." });
-    }
-  } else {
-    response.status(400).json({ code: 2, message: "JWT not sent." });
-  }
+  checkAccessToken(request, response,
+    (email: string) => response.status(200).json({ code: 0, email: email }));
 }
 
 export function getVehicleUserInfo(request: express.Request, response: express.Response) {
-  const accessToken = request.header("x-access-token");
-  if (accessToken) {
-    const result = isJwtCorrect(accessToken);
-    if (result.ok && result.email) {
-      userService.getVehicleUserInfo(result.email).then(data => response.status(200).json(data));
-    } else {
-      response.status(400).json({ code: 1, message: "Bad JWT." });
-    }
-  } else {
-    response.status(400).json({ code: 2, message: "JWT not sent." });
-  }
+  checkAccessToken(request, response,
+    (email: string) => userService.getVehicleUserInfo(email).then(data => response.status(200).json(data)));
 }
 
 export function getUserPaymentsInfo(request: express.Request, response: express.Response) {
-  const accessToken = request.header("x-access-token");
-  if (accessToken) {
-    const result = isJwtCorrect(accessToken);
-    if (result.ok && result.email) {
-      userService.getUserPaymentsInfo(result.email).then(data => response.status(200).json(data));
-    } else {
-      response.status(400).json({ code: 1, message: "Bad JWT." });
-    }
-  } else {
-    response.status(400).json({ code: 2, message: "JWT not sent." });
-  }
+  checkAccessToken(request, response,
+    (email: string) => userService.getUserPaymentsInfo(email).then(data => response.status(200).json(data)));
 }
 
 export function getUserStatistics(request: express.Request, response: express.Response) {
+  checkAccessToken(request, response,
+    (email: string) => userService.getUserStatistics(email).then(data => response.status(200).json(data)));
+}
+
+function checkAccessToken(request: express.Request, response: express.Response, callback: (email: string) => any) {
   const accessToken = request.header("x-access-token");
   if (accessToken) {
     const result = isJwtCorrect(accessToken);
     if (result.ok && result.email) {
-      userService.getUserStatistics(result.email).then(data => response.status(200).json(data));
+      callback(result.email);
     } else {
       response.status(400).json({ code: 1, message: "Bad JWT." });
     }
