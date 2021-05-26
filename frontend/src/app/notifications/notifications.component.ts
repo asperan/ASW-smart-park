f8479ca96ccc7fa3f8e499d5e706b334779302cb
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-26 16:20:30 +0200

    Adds simple class for component

---
+++ frontend/src/app/notifications/notifications.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "notification-icon",
+  templateUrl: "./notifications.component.html",
+  styleUrls: ["./notifications.component.css"],
+})
+export class NotificationIconComponent {
+  constructor() { }
+}
