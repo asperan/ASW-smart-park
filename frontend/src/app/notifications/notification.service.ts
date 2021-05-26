765868ba91809c3e988d6f176a8caebc385880a9
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-26 16:59:29 +0200

    Adds notification service

---
+++ frontend/src/app/notifications/notification.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { TokenManagerService } from "../access-token/token-manager";
+
+@Injectable({
+  providedIn: "root"
+})
+export class NotificationService {
+
+  constructor(private http: HttpClient, private tokenManagerService: TokenManagerService) { }
+
+  async getUnreadNotificationCount() {
+    return this.http.get("http://localhost:3000/api/notifications/unread", { headers: { "x-access-token": await this.tokenManagerService.getToken() } }).toPromise();
+  }
+
+  async getUserNotificationSummary() {
+    return this.http.get("http://localhost:3000/api/notifications", { headers: { "x-access-token": await this.tokenManagerService.getToken() } }).toPromise();
+  }
+}
