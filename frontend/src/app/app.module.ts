e05ecbb43d7341de674dfb08e2b3ceaec5c736dd
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-27 14:38:47 +0200

    Moves the notification icon component in its new folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { PaymentTabComponent } from './pages/user-page/payment-tab/payment-tab.c
 import { StatisticsTabComponent } from './pages/user-page/statistics-tab/statistics-tab.component';
 import { NgStorageModule } from 'ng-storage-local';
 import { PricePipe } from './pipes/prices.pipe';
-import { NotificationIconComponent } from "./notifications/notification-icon.component";
+import { NotificationIconComponent } from "./notifications/notification-icon/notification-icon.component";
 
 
 @NgModule({
