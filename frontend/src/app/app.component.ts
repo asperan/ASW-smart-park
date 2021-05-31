0aeeb61a6ee7dca6e05d2cb426f58e496b61e6af
Francesco Agnoletti <francesco.agnoletti3@studio.unibo.it>
2021-05-31 11:27:04 +0100

    Separated styles and components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,17 +3,13 @@ import { SimpleService } from './simple-service.service';
 
 @Component({
   selector: 'app-root',
- // templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   template: `
-    <!-- Header -->
-    <app-header></app-header>
 
     <!-- routes will be rendered here -->
     <router-outlet></router-outlet>
 
-    <!-- Footer -->
-    <app-footer></app-footer>
   `,
 })
 export class AppComponent {
