6661fdb1cc7ff19e8f7e57faa421c4f9c3fb5511
Alex Speranza <alex.speranza@studio.unibo.it>
2021-05-31 14:59:45 +0200

    Adds simple notification page component

---
+++ frontend/src/app/notifications/notification-page/notification-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+import { NotificationService } from "../notification.service";
+
+@Component({
+  selector: "notification-page",
+  templateUrl: "./notification-page.component.html",
+  styleUrls: ["./notification-page.component.css"]
+})
+export class NotificationPageComponent {
+  userNotifications: any[];
+
+  constructor(private notificationService: NotificationService) {
+    this.userNotifications = [];
+  }
+
+  ngOnInit() {
+    this.notificationService.getAllUserNotifications().then((data: any) => this.userNotifications = data.notifications);
+  }
+}
