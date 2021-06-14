94d7037bdb2aad95c66f62ca18fe40f849c8401f
Kyrillos Ntronov <ntronov.kyrillos@gmail.com>
2021-06-14 21:18:17 +0200

    refactor test location

---
+++ backend/src/test/repositories/cities-repository-test.ts
@@ -0,0 +1,35 @@
+describe("Cities MongoDB Repository", function() {
+    describe("Get all cities", function() {
+        it("Retrives all cities", function() {
+
+        });
+        it("Retrives all cities with Mongo error", function() {
+
+        });
+    });
+    describe("Suggest City By Partial Name", function() {
+        it("Suggest Cities", function() {
+
+        });
+        it("Suggest Cities with Mongo error", function() {
+
+        });
+    });
+    describe("Get city by name", function() {
+        it("Retrives city by name", function() {
+
+        });
+        it("Retrives city by name with Mongo error", function() {
+
+        });
+    });
+    describe("Update city parking", function() {
+        it("Updates city parking", function() {
+
+        });
+        it("Updates city parking with Mongo error", function() {
+
+        });
+    });
+    
+});
