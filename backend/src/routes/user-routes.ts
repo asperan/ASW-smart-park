ebbe2ec5b9a4a730b95adc1370e75474e18b4731
Alex Speranza <alex.speranza@studio.unibo.it>
2021-06-15 15:51:42 +0200

    Adds endpoint to update user subscription

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { addUserPayment, getBasicUserInfo, getUserPaymentsInfo, getUserStatistics, getVehicleUserInfo, postUserVehicle, resolvePendingPayment, updateLastNotificationCheck } from "../controllers/user-controller";
+import { addUserPayment, getBasicUserInfo, getUserPaymentsInfo, getUserStatistics, getVehicleUserInfo, postUserVehicle, resolvePendingPayment, updateLastNotificationCheck, updateUserSubscription } from "../controllers/user-controller";
 
 export function setUserRoutes(app: express.Application): void {
   app.get("/api/user/info-basic", getBasicUserInfo);
@@ -12,4 +12,5 @@ export function setUserRoutes(app: express.Application): void {
      .put(resolvePendingPayment);
   app.get("/api/user/info-stats", getUserStatistics);
   app.post("/api/user/update-last-notification-check",  updateLastNotificationCheck);
+  app.post("/api/user/subscription", updateUserSubscription);
 }
