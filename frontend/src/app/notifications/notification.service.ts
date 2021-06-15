abe12bd2ba154cdbfe5d38e0713f152681ee59ab
Alex Speranza <alex.speranza@studio.unibo.it>
2021-06-15 17:11:52 +0200

    Adds user subscription to notification on signin

--- frontend/src/app/notifications/notification.service.ts
+++ frontend/src/app/notifications/notification.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { TokenManagerService } from "../access-token/token-manager";
+import { SwPush } from "@angular/service-worker";
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: "root"
 })
 export class NotificationService {
 
-  constructor(private http: HttpClient, private tokenManagerService: TokenManagerService) { }
+  constructor(private http: HttpClient, private tokenManagerService: TokenManagerService, private swPush: SwPush) { }
 
   async getUnreadNotificationCount() {
     return this.http.get("http://localhost:3000/api/notifications/unread", { headers: { "x-access-token": this.tokenManagerService.getToken() } }).toPromise();
@@ -19,5 +21,10 @@ export class NotificationService {
 
   async getAllUserNotifications() {
     return this.http.get("http://localhost:3000/api/notifications/all", { headers: { "x-access-token": this.tokenManagerService.getToken() } }).toPromise();
+  }
+
+  async subcribeToPushNotification() {
+    const subscription = await this.swPush.requestSubscription({ serverPublicKey: environment.vapidKey });
+    return this.http.post("http://localhost:3000/api/user/subscription", { subscription: subscription }, { headers: { "x-access-token": this.tokenManagerService.getToken() } }).toPromise();
   }
 }
