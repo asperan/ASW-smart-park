256dfaa5c3cc77a38468f19ec51ab7d9c717349b
Alex Speranza <alex.speranza@studio.unibo.it>
2021-06-16 18:04:36 +0200

    Adds function to configure webpush and to send a notification to a user with a custom payload

--- backend/src/services/notification-service.ts
+++ backend/src/services/notification-service.ts
@@ -1,5 +1,11 @@
 import * as notificationRepository from "../repositories/notifications-repository";
 import * as userRepository from "../repositories/users-repository";
+import webpush from "web-push";
+import { getConfig } from "./config";
+
+export function configureWebpush() {
+  webpush.setVapidDetails("mailto:support@smart-parking.unibo.it", getConfig().server.vapidPublicKey, getConfig().server.vapidPrivateKey);
+}
 
 export async function getUserNotifications(userEmail: string, limit?: number) {
   return notificationRepository.getUserNotifications(userEmail, limit);
@@ -8,4 +14,10 @@ export async function getUserNotifications(userEmail: string, limit?: number) {
 export async function countUnreadNotifications(userEmail: string) {
   const lastCheckDate = new Date(await userRepository.getLastNotificationCheck(userEmail));
   return notificationRepository.countUnreadNotifications(userEmail, lastCheckDate);
+}
+
+export async function sendNotification(userEmail: string, payload: any) {
+  const userSubscription = await userRepository.getUserSubscription(userEmail);
+  console.log(userSubscription.userSubscription);
+  return webpush.sendNotification(userSubscription.userSubscription, JSON.stringify(payload));
 }
