00ab6145076e5c5b066b874b90000c6b65c7795b
Alex Speranza <alex.speranza@studio.unibo.it>
2021-06-18 11:25:23 +0200

    Adds a debug function to try sending a notification

--- backend/src/controllers/notification-controller.ts
+++ backend/src/controllers/notification-controller.ts
@@ -19,3 +19,15 @@ export function getUserUnreadNotifications(request: express.Request, response: e
     notificationService.countUnreadNotifications(email).then(num => response.status(200).json({code: 0, count: num}));
   });
 }
+
+// TODO: remove function, it is used only for debugging purpose
+export function sendNotification(request: express.Request, response: express.Response) {
+  // const notificationPayload = notificationService.buildRemainderNotificationPayload("Your parking expires in 5 minutes", {} ); 
+  // const notificationPayload = notificationService.buildGotoNotificationPayload("Entered on a parking spot", "pay-parking", {"parkingId": "parcheggio1"});
+  const notificationPayload = notificationService.buildGotoNotificationPayload("Look at your notification!", "notifications", {});
+  notificationService.sendNotification(request.body.userEmail, notificationPayload).then(result => {
+    response.status(200).json({ code: 0, message: "Notification sent," });
+  }).catch(reason => {
+    response.status(400).json({ code: 1, message: "Failed to send notification." });
+  });
+}
