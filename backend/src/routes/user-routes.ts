b631adf4d9f3fb88927486bf720b6115ebeb7c06
Alex Speranza <alex.speranza@studio.unibo.it>
2021-06-19 18:56:35 +0200

    Adds routes for removing a linked vehicle and for handling the vehicle currently used by a user

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,16 +1,20 @@
 import express from "express";
-import { addUserPayment, getBasicUserInfo, getUserPaymentsInfo, getUserStatistics, getVehicleUserInfo, postUserVehicle, resolvePendingPayment, updateLastNotificationCheck, updateUserSubscription } from "../controllers/user-controller";
+import { addUserPayment, deleteUserVehicle, getBasicUserInfo, getUserPaymentsInfo, getUserStatistics, getVehicleLinkedToUser, getVehicleUserInfo, linkUserToVehicle, postUserVehicle, resolvePendingPayment, updateLastNotificationCheck, updateUserSubscription } from "../controllers/user-controller";
 import { validateAccessToken } from "../middleware/token-auth";
 
 export function setUserRoutes(app: express.Application): void {
   app.get("/api/user/info-basic", validateAccessToken, getBasicUserInfo);
   app.route("/api/user/info-vehicles").all(validateAccessToken)
      .get(getVehicleUserInfo)
-     .post(postUserVehicle);
+     .post(postUserVehicle)
+     .delete(deleteUserVehicle);
   app.route("/api/user/info-payments").all(validateAccessToken)
      .get(getUserPaymentsInfo)
      .post(addUserPayment)
      .put(resolvePendingPayment);
+  app.route("/api/user/linked-vehicle").all(validateAccessToken)
+     .get(getVehicleLinkedToUser)
+     .post(linkUserToVehicle);
   app.get("/api/user/info-stats", validateAccessToken, getUserStatistics);
   app.post("/api/user/update-last-notification-check", validateAccessToken, updateLastNotificationCheck);
   app.post("/api/user/subscription", validateAccessToken, updateUserSubscription);
