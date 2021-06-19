ede646506dc080e2613a356e9f869cdf1df30d50
Alex Speranza <alex.speranza@studio.unibo.it>
2021-06-19 13:31:10 +0200

    Adds function to link a vehicle to a user

--- frontend/src/app/pages/user-page/user-services/vehicle-info.service.ts
+++ frontend/src/app/pages/user-page/user-services/vehicle-info.service.ts
@@ -12,4 +12,8 @@ export class VehicleInfoService {
   async requestVehicleInfos(): Promise<any> {
     return this.http.get(environment.baseUrl + "/user/info-vehicles", {headers: {"x-access-token": this.tokenManagerService.getToken()}}).toPromise();
   }
+
+  async linkUserToVehicle(vehicleId: string, vehicleName: string): Promise<any> {
+    return this.http.post(environment.baseUrl + "/user/info-vehicles", {vehicleId: vehicleId, vehicleName: vehicleName}, {headers: {"x-access-token": this.tokenManagerService.getToken()}}).toPromise();
+  }
 }
