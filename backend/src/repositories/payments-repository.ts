a0452c964a86a92604b18a498d66891181787bcc
Alex Speranza <alex.speranza@studio.unibo.it>
2021-06-23 16:11:27 +0200

    Removes unused function

--- backend/src/repositories/payments-repository.ts
+++ backend/src/repositories/payments-repository.ts
@@ -4,10 +4,6 @@ export async function addPermanence(userEmail: string, vehicleId: string, parkin
   return (await mongoClient.db.collection("parkingstays").insertOne({userEmail: userEmail, vehicleId: vehicleId, parkingSpotId: parkingSpotId, entryDate: entryDate, payedUntil: payedUntil, payment: payment})).result.ok === 1;
 }
 
-export async function resolvePendingPayment(userEmail: string, parkingId: string, date: Date): Promise<boolean> {
-  return (await mongoClient.db.collection("payments").updateOne({userEmail: userEmail, parkingId: parkingId, date: date, pending: true}, {$set: {pending: false}})).result.ok === 1;
-}
-
 export async function getUserPermanences(email: string): Promise<any[]> {
   return await mongoClient.db.collection("parkingstays").find({ userEmail: email }).sort("entryDate", -1).toArray();
 }
