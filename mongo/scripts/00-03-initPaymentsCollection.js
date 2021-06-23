ff3c0206ee2a064279550dc9bf9fe991db3d15a5
Alex Speranza <alex.speranza@studio.unibo.it>
2021-06-23 16:11:56 +0200

    Removes payment collection as they are included in parkingstays

--- mongo/scripts/00-03-initPaymentsCollection.js
+++ mongo/scripts/00-03-initPaymentsCollection.js
@@ -1,36 +1,3 @@
-db.createCollection("payments", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      required: ["userEmail", "parkingId", "date", "amount", "pending"],
-      properties: {
-        userEmail: {
-          bsonType: "string",
-          description: "must be an existing user email"
-        },
-        parkingId: {
-          bsonType: "string",
-          description: "must be an existing parking id"
-        },
-        date: {
-          bsonType: "date",
-          description: "the timestamp when the payment has been requested"
-        },
-        amount: {
-          bsonType: "int",
-          description: "the amount to pay in cents"
-        },
-        pending: {
-          bsonType: "boolean",
-          description: "whether the payment is waiting the exit of the user from the parking"
-        }
-      }
-    }
-  }
-});
-
-db.payments.createIndex({ userEmail: 1 });
-
 db.createCollection("parkingstays", {
   validator: {
     $jsonSchema: {
