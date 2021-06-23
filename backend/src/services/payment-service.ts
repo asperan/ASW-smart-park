ecb22b2822142e71fa8a21dc43c829ff340eda0f
Alex Speranza <alex.speranza@studio.unibo.it>
2021-06-23 12:31:35 +0200

    Updates function for adding a permanence

--- backend/src/services/payment-service.ts
+++ backend/src/services/payment-service.ts
@@ -1,8 +1,8 @@
 import * as paymentRepository from "../repositories/payments-repository";
 
 // TODO: On payment add remainder notifications
-export async function addPermanence(email: string, parkingId: string, date: Date, amount: number): Promise<boolean> {
-  return paymentRepository.addPermanence(email, parkingId, date, amount);
+export async function addPermanence(email: string, vehicleId: string,  parkingId: string, entryDate: Date, payedUntil: Date, payment: {paymentId: string, amount: number}): Promise<boolean> {
+  return paymentRepository.addPermanence(email, vehicleId, parkingId, entryDate, payedUntil, payment);
 }
 
 export async function resolvePendingPayment(email: string, parkingId: string, date: Date): Promise<boolean> {
