ccb234a27d930a201a96284ee466c2daa3a81358
Alex Speranza <alex.speranza@studio.unibo.it>
2021-06-23 16:09:41 +0200

    Removes debug functions

--- backend/src/controllers/notification-controller.ts
+++ backend/src/controllers/notification-controller.ts
@@ -15,15 +15,3 @@ export function getAllUserNotifications(request: express.Request, response: expr
 export function getUserUnreadNotifications(request: express.Request, response: express.Response) {
   if (request.userEmail) notificationService.countUnreadNotifications(request.userEmail).then(num => response.status(200).json({code: 0, count: num}));
 }
-
-// TODO: remove function, it is used only for debugging purpose
-export function sendNotification(request: express.Request, response: express.Response) {
-  // const notificationPayload = notificationService.buildRemainderNotificationPayload("Your parking expires in 5 minutes", {} ); 
-  // const notificationPayload = notificationService.buildGotoNotificationPayload("Entered on a parking spot", "pay-parking", {"parkingId": "parcheggio1"});
-  const notificationPayload = notificationService.buildGotoNotificationPayload("Look at your notification!", "notifications", {});
-  notificationService.sendNotification(request.body.userEmail, notificationPayload).then(result => {
-    response.status(200).json({ code: 0, message: "Notification sent," });
-  }).catch(reason => {
-    response.status(400).json({ code: 1, message: "Failed to send notification." });
-  });
-}
