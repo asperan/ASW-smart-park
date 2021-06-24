9bfb62897f0b9cfa621bd152d44533bc655225a6
Kyrillos Ntronov <ntronov.kyrillos@gmail.com>
2021-06-24 21:53:39 +0200

    fix reviiews in db

---
+++ mongo/scripts/01-05-initReviewsCollection.js
@@ -0,0 +1,11 @@
+db.reviews.insertMany([
+{
+    "parkingId": NumberInt(1),
+    "userEmail": "user1@admin.com",
+    "rating": NumberInt(8)
+},
+{
+    "parkingId": NumberInt(1),
+    "userEmail": "user2@admin.com",
+    "rating": NumberInt(6)
+}])
