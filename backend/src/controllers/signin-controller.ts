484156e797f7fe53b1488e2cf9e098d6c63b3d68
Alex Speranza <alex.speranza@studio.unibo.it>
2021-06-24 11:00:06 +0200

    Adds check for body parameters

--- backend/src/controllers/signin-controller.ts
+++ backend/src/controllers/signin-controller.ts
@@ -3,13 +3,17 @@ import * as userService from "../services/user-service";
 import * as userAuth from "../services/user-auth";
 
 export function signinUser(request : express.Request, response: express.Response) {
-  const userEmail = request.body.email;
-  const userPassword = request.body.password;
-  userService.checkUserPassword(userEmail, userPassword).then(ok => {
-    if(ok) {
-      response.status(200).json({code: 0, message: "Signed in successfully", access_token: userAuth.generateNewJwt(userEmail)});
-    } else {
-      response.status(400).json({code: 4, message: "User/password match not found"});
-    }
-  });
+  if (request.body.email && request.body.password) {
+    const userEmail = request.body.email;
+    const userPassword = request.body.password;
+    userService.checkUserPassword(userEmail, userPassword).then(ok => {
+      if (ok) {
+        response.status(200).json({ code: 0, message: "Signed in successfully", access_token: userAuth.generateNewJwt(userEmail) });
+      } else {
+        response.status(400).json({ code: 4, message: "User/password match not found" });
+      }
+    });
+  } else {
+    response.status(400).json({ code: 2, message: "User/password not sent." });
+  }
 }
