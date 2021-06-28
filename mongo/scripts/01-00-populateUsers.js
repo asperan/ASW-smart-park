672a9e1aa511e9b222766712075c86d570239d4d
Alex Speranza <alex.speranza@studio.unibo.it>
2021-06-28 10:37:08 +0200

    Adds stub user for demo

---
+++ mongo/scripts/01-00-populateUsers.js
@@ -0,0 +1,8 @@
+db.users.insertOne({
+  email: "user@user.com",
+  password: "93e8e9cedfb995a4e8239c8c3c29eb90f41b1e6be063d3e297fb56f408489977",
+  salt: "f48f1c8a20915a0e",
+  lastNotificationCheck: new Date(),
+  linkedVehicles: [],
+  userSubscription: {}
+});
